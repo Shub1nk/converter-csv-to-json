314cb2f7e7f014f14c962349029c739720279fb1
Kirill Stolyarov <kstolyarov@alfabank.ru>
2020-06-25 13:27:27 +0500

    added new csv data type

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ const pathDesktopJSON = __dirname + "/json/desktop/";
 const pathMobileJSON = __dirname + "/json/mobile/";
 
 const allFiles = fs.readdirSync(`${__dirname}/csv`);
-const files = allFiles.filter((name) => name !== ".gitkeep");
+const files = allFiles.filter(
+  (name) => name !== ".gitkeep" && name !== ".DS_Store"
+);
 
 const YEAR = 12;
 
@@ -16,23 +18,42 @@ const makeIfNotExists = (dir) => {
 };
 
 files.forEach((file) => {
-  CSVToJSON()
+  CSVToJSON({
+    delimiter: [",", ";"],
+  })
     .fromFile(`${__dirname}/csv/${file}`)
     .then((source) => {
       const updatedDesktopSource = source.map((item) => {
-        const paramDate = item["Дата"]
-          .replace(/(\d{2}).(\d{2}).(\d{4})/, "$3-$2-$1")
-          .split("-");
+        // TODO можно объединить со следующим блоком if
+        if (item.hasOwnProperty("date")) {
+          const paramDate = item["date"]
+            .replace(/(\d{4})(\d{2})(\d{2})/, "$1-$2-$3")
+            .split("-");
 
-        return {
-          p: Number(item["Стоимость"]),
-          d: Date.UTC(paramDate[0], paramDate[1] - 1, paramDate[2]),
-        };
+          return {
+            p: Number(item["price"]),
+            d: Date.UTC(paramDate[0], paramDate[1] - 1, paramDate[2]),
+          };
+        }
+        if (item.hasOwnProperty("Дата")) {
+          const paramDate = item["Дата"]
+            .replace(/(\d{2}).(\d{2}).(\d{4})/, "$3-$2-$1")
+            .split("-");
+
+          return {
+            p: Number(item["Стоимость"]),
+            d: Date.UTC(paramDate[0], paramDate[1] - 1, paramDate[2]),
+          };
+        }
       });
 
-      const lastDate = new Date(updatedDesktopSource[updatedDesktopSource.length - 1].d);
+      const lastDate = new Date(
+        updatedDesktopSource[updatedDesktopSource.length - 1].d
+      );
       const yearAgo = new Date(lastDate).setMonth(lastDate.getMonth() - YEAR);
-      const updatedMobileSource = updatedDesktopSource.filter((item) => item.d >= yearAgo);
+      const updatedMobileSource = updatedDesktopSource.filter(
+        (item) => item.d >= yearAgo
+      );
 
       makeIfNotExists(pathDesktopJSON);
       makeIfNotExists(pathMobileJSON);
